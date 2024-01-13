f7f02186700dd15e98ddec8bf1a87b7e9c82feaa
Jamie Corstorphine <jvcorstorphine@gmail.com>
2024-01-13 13:50:47 +1300

    feat: added sanity schema for executives

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,17 +1,28 @@
 import { getProjects } from "@/sanity/sanity-utils";
+import Image from "next/image";
 
 //export const revalidate = 60;
 
 export default async function Home() {
     const projects = await getProjects();
 
-    //console.log("projects: ", projects);
+    console.log("projects: ", projects);
 
     return (
         <div className="max-w-5x1 mx-auto">
-            <h1>ASW Website</h1>
+            <h1>ASW Website test</h1>
             {projects.map((project) => (
-                <div key={project.name}>{project.name}</div>
+                <div key={project.name}>{project.name}
+                {project.image && ( 
+                    <Image
+                        src={project.image}
+                        alt={project.name}
+                        width={250}
+                        height={100}
+                        />
+                    )}
+                    </div>
+                
             ))}
         </div>
     );
---
+++ sanity/schemas/exec-schema.ts
@@ -0,0 +1,30 @@
+const executive = {
+    name: 'executive',
+    title: 'Executives',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {hotspot: true},
+            fields: [
+                {
+                name: 'alt',
+                title: 'Alt',
+                type: 'string'
+                }
+            ]
+        },
+        {
+            name: 'url',
+            title: 'URL',
+            type: 'url'
+        }
+    ]
+}
