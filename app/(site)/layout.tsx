399cc920f344f522a428dd7c53c632d6ca030356
Jamie Corstorphine <jvcorstorphine@gmail.com>
2024-02-01 14:57:14 +1300

    Fix: Footer is now on the layout page to make it global.

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import NavBar from "./component/NavBar";
+import Footer from "./component/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             <body className={inter.className}>
                 <header><NavBar/></header>
                 <main>{children}</main>
+                <Footer/>
             </body>
 
         </html>
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,7 +2,7 @@
 import ASWIntroduction from "./component/LandingPage/ASWIntroduction";
 import GeneralSponsors from "./component/LandingPage/GeneralSponsors";
 import OurEvents from "./component/LandingPage/OurEvents";
-import NavBar from "./component/NavBar";
+
 
 export default function Home() {
     return (
