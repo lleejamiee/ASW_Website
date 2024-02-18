08e00b0dcd163e26aee5632b39e25a7f33635579
jamielee <85136381+lleejamiee@users.noreply.github.com>
2024-02-18 14:41:18 +1300

    feat[MobileEventsPage]: Default past event set to most current

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,5 +1,4 @@
 import { Project } from "@/types/Project";
-import { Page } from "@/types/Page";
 import { Executive, Executives } from "@/types/Executive";
 import { createClient, groq } from "next-sanity";
 import clientConfig from "./config/client-config";
@@ -39,32 +38,6 @@ export async function getProject(slug: string): Promise<Project> {
                 "altText": altText
             },
             url,
-            content
-        }`,
-        { slug }
-    );
-}
-
-// Returns all pages
-export async function getPages(): Promise<Page[]> {
-    return createClient(clientConfig).fetch(
-        groq`*[_type == "page"]{
-            _id,
-            _createdAt,
-            title,
-            "slug": slug.current
-        }`
-    );
-}
-
-// Returns single page
-export async function getPage(slug: string): Promise<Page> {
-    return createClient(clientConfig).fetch(
-        groq`*[_type == "page"&& slug.current == $slug][0]]{
-            _id,
-            _createdAt,
-            title,
-            "slug": slug.current,
             content
         }`,
         { slug }
--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,17 +1,8 @@
-import page from "./page-schema";
 import project from "./project-schema";
 import { executive, executives } from "./exec-schema";
 import { event, events } from "./events-schema";
 import memberSignUp from "./signup-schema";
 
-const schemas = [
-    project,
-    page,
-    executives,
-    executive,
-    events,
-    event,
-    memberSignUp,
-];
+const schemas = [project, executives, executive, events, event, memberSignUp];
 
 export default schemas;
